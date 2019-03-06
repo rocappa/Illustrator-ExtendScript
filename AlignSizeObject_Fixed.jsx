8c84c7bfdfc209196844eeee8d574c6b5529c510
nakagawa <nakagawa@nishikawa.jp>
2019-03-06 16:49:27 +0900

    no message

--- AlignSizeObject_Fixed.jsx
+++ AlignSizeObject_Fixed.jsx
@@ -1,12 +1,12 @@
 pt=25.4/72
 var dia = new Window("dialog", "オブジェクトの大きさを揃える", [0,0,250,180]);
-    dia.pane1 = dia.add("panel",[5,25,240,65],"サイズの基準");
-    dia.radi1 = dia.pane1.add("radiobutton",[10,5,60,20],"タテ", );
-    dia.radi2 = dia.pane1.add("radiobutton",[70,5,120,20],"ヨコ");
-    dia.pane2 = dia.add("panel",[5,75,240,125],"サイズは");
+    dia.pane1 = dia.add("panel",[5,25,240,65],"Reference size");
+    dia.radi1 = dia.pane1.add("radiobutton",[10,5,60,20],"Height", );
+    dia.radi2 = dia.pane1.add("radiobutton",[70,5,120,20],"Width");
+    dia.pane2 = dia.add("panel",[5,75,240,125],"Reference size");
     dia.tex1 = dia.pane2.add("edittext",[10,10,100,30]);
     dia.add("statictext",[110,100,240,120],"mm");
-    dia.bot1 = dia.add("button",[10,140,100,170],"実行", { name:"ok"});
+    dia.bot1 = dia.add("button",[10,140,100,170],"Run", { name:"ok"});
     dia.radi1.value=true;
     dia.center();
     dia.show();
